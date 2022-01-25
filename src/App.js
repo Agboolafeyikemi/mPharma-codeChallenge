477e21934e10c93350a06911f042b243bd162163
Agboolafeyikemi <agboolafeyikemi93@gmail.com>
2022-01-25 23:12:12 +0100

    Genesis commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,214 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Products from "./components/Products";
+import React, { useState, useEffect } from "react";
+// import axios from 'axios'
+import Loader from "./components/Loader";
+import AddModal from "./components/AddModal";
+import ConfirmationModal from "./components/ConfirmationModal";
+import Toastify from "./components/Toastify";
+import { toast } from "react-toastify";
+//import { useSelector, useDispatch } from 'react-redux'
+
+let initData = [];
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [liveData, setLiveData] = useState(initData);
+  const [isLoading, setIsloading] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [isOpen, setConfirmModalOpen] = useState(false);
+  const [modalTitle, setModalTitle] = useState("Add Product");
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+  const [showUpdate, setShowUpdate] = useState(false);
+  const [hideSave, setHideSave] = useState(false);
+  const [toastrMessage, setToastMessage] = useState("Notification");
+
+  //const dispatch = useDispatch()
+  //const seedData = useSelector(state => products)
+  //console.log("Seed Data:" , seedData)
+
+  const getData = async () => {
+    try {
+      const result = await fetch(
+        "https://www.mocky.io/v2/5c3e15e63500006e003e9795"
+      );
+      const data = await result.json();
+      if (data.products) {
+        let formattedData = data.products.map((item) => {
+          return {
+            id: item.id,
+            name: item.name,
+            currentPrice: item.prices[0].price,
+            prevPrice: item.prices[1].price,
+          };
+        });
+        initData = formattedData;
+
+        console.log("FormattedData:", formattedData);
+        //setting copy of init data to liveData for search implementation
+        setLiveData(initData);
+        //setLiveData(seedData)
+
+        //cacheing in memory for offline use
+        localStorage.setItem("initData", JSON.stringify(initData));
+        setIsloading(false);
+
+        //dispatch({ type: 'UPDATE_DATA', products: initData });
+        setProducts(initData);
+        //setProducts(seedData)
+      }
+    } catch (error) {
+      setIsloading(false);
+      const offlineData = localStorage.getItem("initData");
+      //console.log(offlineData)
+      setLiveData(JSON.parse(offlineData));
+      setProducts(JSON.parse(offlineData));
+      console.log(error);
+    }
+
+    // axios
+    //   .get('http://www.mocky.io/v2/5c3e15e63500006e003e9795')
+    //   .then((res) => {
+    //     //console.log('Data:', res.data.products)
+    //     res.data.products.map((item) => {
+    //   let newObj = {
+    //     id: item.id,
+    //     name: item.name,
+    //     currentPrice: item.prices[0].price,
+    //     prevPrice: item.prices[1].price,
+    //   }
+    //   initData.push(newObj)
+    // })
+    //     console.log(initData)
+    //     setProducts(initData)
+    //   })
+    //   .catch((err) => {
+    //     console.log(err)
+    //   })
+    //   .finally(console.log('done'))
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const handleAddProductModal = () => {
+    setOpen(!open);
+    setProductName("");
+    setProductPrice("");
+    setModalTitle("Add Product");
+    setShowUpdate(false);
+    setHideSave(false);
+  };
+
+  const notify = (toastrMessage, type) => {
+    setToastMessage(toastrMessage);
+    switch (type) {
+      case "warning":
+        toast.warning(toastrMessage, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        break;
+
+      case "success":
+        toast.success(toastrMessage, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        break;
+
+      default:
+        toast.info(toastrMessage, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar
+        setProducts={setProducts}
+        handleAddProductModal={handleAddProductModal}
+        data={initData}
+        liveData={liveData}
+        setLiveData={setLiveData}
+      />
+
+      <Toastify />
+      <ConfirmationModal
+        isOpen={isOpen}
+        setConfirmModalOpen={setConfirmModalOpen}
+        products={products}
+        setProducts={setProducts}
+        liveData={liveData}
+        setLiveData={setLiveData}
+        notify={notify}
+        setToastMessage={setToastMessage}
+      />
+
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Products
+          products={products}
+          //products={payload}
+          setOpen={setOpen}
+          modalTitle={modalTitle}
+          setModalTitle={setModalTitle}
+          productName={productName}
+          setProductName={setProductName}
+          productPrice={productPrice}
+          setProductPrice={setProductPrice}
+          showUpdate={showUpdate}
+          setShowUpdate={setShowUpdate}
+          hideSave={hideSave}
+          setHideSave={setHideSave}
+          setConfirmModalOpen={setConfirmModalOpen}
+          isOpen={isOpen}
+        />
+      )}
+      <AddModal
+        handleAddProductModal={handleAddProductModal}
+        open={open}
+        products={products}
+        //products={payload}
+        setProducts={setProducts}
+        setOpen={setOpen}
+        data={initData}
+        liveData={liveData}
+        setLiveData={setLiveData}
+        modalTitle={modalTitle}
+        setModalTitle={setModalTitle}
+        productName={productName}
+        setProductName={setProductName}
+        productPrice={productPrice}
+        setProductPrice={setProductPrice}
+        showUpdate={showUpdate}
+        setShowUpdate={setShowUpdate}
+        hideSave={hideSave}
+        setConfirmModalOpen={setConfirmModalOpen}
+        notify={notify}
+        toastrMessage={toastrMessage}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+import AddModal from "./components/AddModal";
+import {
+  multiply,
+  makeLowercase,
+} from "../src/HelperFunctions/HelperFunctions";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("Multiply", () => {
+  expect(multiply(2, 10)).toBe(20);
+});
+
+test("makeLowerCase", () => {
+  expect(makeLowercase("Mr Stark")).toBe("mr stark");
+});
+
+it("checkAddModalRender", () => {
+  const { queryByTitle } = render(<AddModal />);
+  const linkElement = screen.getByText("Add Product");
   expect(linkElement).toBeInTheDocument();
+  // const productName = queryByTitle('productName')
+  // console.log(productName)
+  //expect(productName).toBeTruthy();
+});
+
+describe("clickSaveButton", () => {
+  it("onClick", () => {
+    const { queryByDisplayValue } = render(<AddModal />);
+    const saveButton = queryByDisplayValue("Save");
+    expect(saveButton.innerHTML).toBe("Save");
+    fireEvent.click(saveButton);
+  });
 });
